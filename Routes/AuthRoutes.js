d4f6399d259bde5e996e9a17e8b71acf13240ceb
Vishnu <vishnunair20012112@gmail.com>
2025-09-03 23:05:57 +0530

    middleware added to / root route

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -4,7 +4,7 @@ import express  from "express";
 
 const router = express.Router();
 
-router.get("/",(req,res)=>{res.redirect("/register")});
+router.get("/",authMiddleware.verifyTokensForAuthRoute,(req,res)=>{res.redirect("/register")});
 router.get("/login",authMiddleware.verifyTokensForAuthRoute,(req,res)=>{res.render("registerLoginView/login/login")});
 router.get("/register",authMiddleware.verifyTokensForAuthRoute,(req,res)=>{res.render("registerLoginView/register/register")});
 router.get("/homePage",authMiddleware.verifyTokensForUserRoute,(req,res)=>{res.render("userView/user_registered",{userdata:req.user.userName})});
